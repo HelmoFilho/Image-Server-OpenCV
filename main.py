09a396b9ab00187c9a6b035d4fbab3cc704d1b67
HelmoFilho <helmofilho09@gmail.com>
2023-03-05 21:12:25 -0300

    Modified development and production startup

--- main.py
+++ main.py
@@ -3,51 +3,30 @@ from waitress import serve
 
 ## -- Importing Internal Modules -- ##
 from app.routing import app
+import app.config as cnfg
 
 def main():
     """
     Main program function
     """
 
-    # Pega a variável do sistema criada no import do app
-    choice = unidecode(os.environ.get("PSERVER_SI").lower())
-    port = int(os.environ.get("MAIN_PORT_SI"))
-    close_all_sessions()
-
     # Server utilizado para produção 
-    if choice == "production":
-
-        port = int(os.environ.get("MAIN_PORT_SI"))
-        threads = int(os.environ.get("PROD_THREADS_SI"))
-        connection_limit = int(os.environ.get("PROD_CON_LIMIT_SI"))
-        cleanup_interval = float(os.environ.get("PROD_CLEANUP_INTERVAL_SI"))
-        channel_timeout = float(os.environ.get("PROD_CHANNEL_TIMEOUT_SI"))
+    if cnfg.SERVER == "production":
 
         dict_connection = {
             "app": app,
             "host": "0.0.0.0",
-            "port": port,
-            "threads": threads,
-            "connection_limit": connection_limit,
-            "cleanup_interval": cleanup_interval,
-            "channel_timeout": channel_timeout
+            "port": cnfg.PORT,
+            "threads": cnfg.THREADS,
+            "connection_limit": cnfg.CONNECTION_LIMIT,
+            "cleanup_interval": cnfg.CLEANUP_INTERVAL,
+            "channel_timeout": cnfg.CHANNEL_TIMEOUT,
         }
 
         serve(**dict_connection)
     
-    # Server utilizado para desenvolvimento 
-    elif choice == "development":
-        print(f"Starting server in DEVELOPMENT enviroment")
-        app.run(host = "0.0.0.0", port = port)    
-    
-    # Server utilizado para teste (inútil)
-    elif choice in ["testing", "debug"]:
-        print(f"Starting server in TESTING enviroment")
-        app.run()
-
     else:
-        raise Exception("PSERVER com valor invalido")
-
+        app.run(host = "0.0.0.0", port = cnfg.PORT,)    
 
 if __name__ == "__main__":
     main()
