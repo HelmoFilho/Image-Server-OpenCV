1c51fc5e9412db82d999144e3990bac70f0f4427
HelmoFilho <helmofilho09@gmail.com>
2023-03-05 21:13:57 -0300

    Added more routes and error_handlers

--- app/routing.py
+++ app/routing.py
@@ -1,9 +1,87 @@
 ## -- Importing External Modules -- ##
-from flask import make_response, json, g, wrappers
+from flask import make_response, json, wrappers, g, request
 from timeit import default_timer as timer
+from functools import wraps
 
 ## -- Importing Internal Modules -- ##
+from app.resources.folder_structure import folder_structure_get, folder_structure_post
+from app.resources.image_serving import image_serving_get
 from app.server import app
+
+
+def add_body(generic_function: callable):
+    """
+    Add the json data or url params to the data received by the endpoint
+
+    Args:
+        generic_function (callable): The endpoint
+
+    Returns:
+        any: Whatever the endpoint returns
+    """
+
+    @wraps(generic_function)
+    def wrapper(*args, **kwargs):
+
+        request_data = {}
+
+        if request.args:
+            hold_args = {
+                str(key).lower(): value 
+                for key, value in request.args.items()
+            }
+
+            request_data.update(hold_args)
+
+        if request.data:
+        
+            json_data = request.get_json()
+            if json_data and isinstance(json_data, dict):
+
+                hold_json = {
+                    str(key).lower(): value 
+                    for key, value in json_data.items()
+                }
+                request_data.update(hold_json)
+
+        if request_data:
+            kwargs["request"] = request_data
+              
+        response = generic_function(*args, **kwargs)
+        return response
+
+    return wrapper
+
+
+def add_files(generic_function: callable):
+    """
+    Add the form-data to the request data
+
+    Args:
+        generic_function (callable): The endpoint
+
+    Returns:
+        any: Whatever the endpoint returns
+    """
+
+    @wraps(generic_function)
+    def wrapper(*args, **kwargs):
+
+        request_data = {}
+
+        hold = dict(request.files)
+    
+        for key, value in hold.items():
+            request_data[str(key).lower()] = value
+
+        if request_data:
+            kwargs["files"] = request_data
+              
+        response = generic_function(*args, **kwargs)
+        return response
+
+    return wrapper
+
 
 # Cors handler
 def add_cors_return(resp) -> json:
@@ -21,15 +99,52 @@ def add_cors_return(resp) -> json:
     return response
 
 
-# 404 response
+# Generic
+@app.errorhandler(Exception)
+def internal_server_error(e):
+
+    # You can add some logging here or even add some error handlers for especific cases
+    return {
+        "status": False,
+        "message": "Internal Server Error"
+    }
+
+# Not found
 @app.errorhandler(404)
-def not_found(e):
-    return {"response": "not found"}, 404
+def not_found_error(e):
+
+    # You can add some logging here or even add some error handlers for especific cases
+    return {
+        "status": False,
+        "message": "Not Found"
+    }
+
+# Especific
+@app.errorhandler(400)
+def bad_request_error(e):
+
+    # You can add some logging here or even add some error handlers for especific cases
+    description = e.description
+    message: str = None
+
+    if isinstance(description, str):
+        message = description
+
+    elif isinstance(description, dict):
+        message = description.get('message') if description.get("message") else description
+
+    else:
+        message = description
+
+    return {
+        "status": False,
+        "message": message
+    }
 
 
 # Before request
 @app.before_request
-def before_request(resp):
+def before_request():
     
     g.start = timer()
 
@@ -40,45 +155,41 @@ def after_request(resp):
 
     g.finish = timer()
     
-    if int(resp.status_code) == 404:
-        return {"response": "not found"}, 404
-    
     resp = add_cors_return(resp)
-    resp.headers.add("X-Process-Time", "{:.5f}".format(g.finish - g.start))
+
+    if g.get("start") and g.get("finish"):
+        resp.headers.add("X-Process-Time", "{:.5f}".format(g.finish - g.start))
+    
     return resp
 
 
 #### routing
-@app.route("/<path:direcao>", methods = ['GET'])
-def servir_arquivo(**kwargs) -> wrappers.Response:
+@app.route("/image/<path:full_path>", methods = ['GET'])
+@add_body
+def image_service(**kwargs) -> wrappers.Response:
     """
-    Retorna o arquivo requerido pelo front-end
+    Returns a saved image
 
     kwargs:
-        tipo (str): Tipo de arquivo.
-        direcao (str): direcao para a imagem.
-
-    Returns:
-        wrappers.Response: Resultado da transação
+        full_path (str): Path to the image.
     """
 
-    # Pegando os dados da URL
-    tipo = str(kwargs.get("tipo")).lower()
+    return image_serving_get(**kwargs)
 
-    # Criando a rota principal do tipo de arquivo
-    root_folder = str(os.environ.get("ROOT_FOLDER_SI"))
-    main_route = f"{root_folder}\\{tipo}"
-    
-    # Checa se rota principal existe
-    if not os.path.exists(main_route):
-        abort(404)
 
-    # Verifica se o tipo é de um dos tipo que esse servidor pode tratar
-    if tipo == "imagens":
-        data = {key: value   for key, value in kwargs.items()   if key != "tipo"}
-        response = si.servir_imagens_get(**data)
+@app.route("/structure", methods = ['GET', "POST"])
+@add_body
+@add_files
+def folder_structure(**kwargs) -> wrappers.Response:
+    """
+    Returns a saved image
+
+    kwargs:
+        full_path (str): Path to the image.
+    """
+
+    if request.method.upper() == 'GET':
+        return folder_structure_get(**kwargs)
     
     else:
-        abort(404)
-    
-    return response
+        return folder_structure_post(**kwargs)
