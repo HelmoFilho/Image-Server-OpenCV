7fd84b0b61354acda1ff3b59dc5ef320e13ada53
HelmoFilho <helmofilho09@gmail.com>
2023-03-05 21:14:17 -0300

    Added more global variables

--- app/config.py
+++ app/config.py
@@ -5,5 +5,11 @@ import dotenv, os
 
 dotenv.load_dotenv("./config/.env")
 
+SERVER = os.getenv("SERVER_IS", "development").lower()
 PORT = int(os.getenv("PORT_IS", 5000))
 MAIN_FOLDER = os.getenv("CHECK_FOLDER_IS", "./images")
+
+THREADS = int(os.getenv("THREAD_IS", 4))
+CONNECTION_LIMIT = int(os.getenv("CONNECTION_LIMIT_IS", 4))
+CLEANUP_INTERVAL = int(os.getenv("CLEANUP_INTERVAL_IS", 4))
+CHANNEL_TIMEOUT = int(os.getenv("CHANNEL_TIMEOUT_IS", 4))
